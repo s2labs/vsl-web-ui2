d928125275c065c4fc37c0c935d6f3fd1cbe1534
Andreas Hubel <andi@saerdnaer.de>
2016-02-05 18:20:03 +0100

    write locations back to server

--- app/controllers/devices.js
+++ app/controllers/devices.js
@@ -3,7 +3,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 //center: {lat: 48.2628, lng: 11.6685},
   center: {lat: 78, lng: -90},
-  lat: 78, lng: -90,
   zoom: 4, 
 
+  actions: {
+    updateLocation(r, e) {
+      let location = e.target.getLatLng();
+      r.set('location', location);
+      r.save();
+    }
+  }
 });
