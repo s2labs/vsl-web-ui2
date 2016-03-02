8242cfc0df021e43312102cc7de57c533f43ac74
Andreas Hubel <andi@saerdnaer.de>
2016-03-02 12:44:33 +0100

    added device list

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('map', { path: '/'});
+  this.route('list', { path: '/list'});
   this.route('test', { path: '/test'});
 });
 
