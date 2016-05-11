dcdab165c05e2bd708b910409b003804945d899d
Andreas Hubel <andi@saerdnaer.de>
2016-05-11 21:26:14 +0200

    introduced device renderer

---
+++ app/components/device-renderer.js
@@ -0,0 +1,10 @@
+// TODO: Rename class from device-renderer to e.g. node-renderer or item-renderer, or whateverta
+
+
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+
+
+});
