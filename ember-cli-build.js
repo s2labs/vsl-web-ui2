e15234d4c8d39b9de86f1e0958710e5ced6fe5c7
Andreas Hubel <andi@saerdnaer.de>
2016-05-11 23:14:06 +0200

    added snippet from https://github.com/miguelcobain/ember-leaflet#production-builds

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,15 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
     var app = new EmberApp(defaults, {
         // Any other options
+        fingerprint: {
+          exclude: [
+            'images/layers-2x.png',
+            'images/layers.png',
+            'images/marker-icon-2x.png',
+            'images/marker-icon.png',
+            'images/marker-shadow.png'
+          ]
+        }
     });
 
 
