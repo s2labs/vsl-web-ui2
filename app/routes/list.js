5a03a88060a24ac7edc7fd3b2f7449527754eae5
Andreas Hubel <andi@saerdnaer.de>
2017-04-07 16:59:29 +0200

    fixed grid view errors

--- app/routes/list.js
+++ app/routes/list.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     // TODO: type per query parameter in der URL setzen?
-    return this.store.query('deviceref', {'typeSearch' : '/gahu/genericDevice'});
+    return this.store.query('deviceref', {'typeSearch' : '/gahu/actuator'});
   },
 
 });
