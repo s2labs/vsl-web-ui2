0f9f8534b60f8a15163db9a12cdb1b62ed664b02
Andreas Hubel <andreas.hubel@tum.de>
2017-04-24 22:11:42 +0200

    added first iteration of a logout button, which propobably does not event work in todays common browsers

--- app/components/topbar-menu.js
+++ app/components/topbar-menu.js
@@ -4,6 +4,10 @@ export default Ember.Component.extend({
 	actions: {
 		reload() {
 			location.reload();
+		},
+       		logout() {
+			// only works for firefox and old versions of chrome, cf. http://stackoverflow.com/questions/9724489/clear-ssl-client-certificate-state-from-javascript/9724667#9724667
+			if (window.crypto) window.crypto.logout();
 		}
 	}
 
